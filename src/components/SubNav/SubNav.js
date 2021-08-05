cf9a5ca3aeeba8646f16eb856cd547930ef2c06f
Elizabeth Bryson <liz@lizbryson.co>
2021-08-05 10:02:09 -0500

    add subnav for subpages

---
+++ src/components/SubNav/SubNav.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Logo from "../../svg/Logo";
+import "./SubNav.css";
+
+const SubNav = () => {
+  return (
+    <header className="siteSubHeader">
+      <nav className="siteSubNav">
+        <ul className="siteSubNav__inner">
+          <li className="siteSubNav__li">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="siteSubNav__li">
+            <a href="#hotels">Hotels</a>
+          </li>
+          <Link to="/" className="siteLogo">
+            <Logo />
+          </Link>
+          <li className="siteSubNav__li">
+            <a href="#transport">Transport</a>
+          </li>
+          <li className="siteSubNav__li">
+            <a
+              href="https://www.zola.com/registry/thehalseywedding"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Registry
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default SubNav;
--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../layout";
+import SubNav from "../components/SubNav/SubNav";
 import RsvpSection from "../components/RsvpSection/RsvpSection";
 
 const Index = () => {
   return (
     <Layout>
+      <SubNav />
       <RsvpSection />
     </Layout>
   );
